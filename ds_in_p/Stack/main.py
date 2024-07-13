6767244b1c883fc916295a5e49f678d248c197ae
Mantej Sokhi <sokhimantej99@gmail.com>
2024-07-13 15:08:15 -0700

    Added files via upload.

---
+++ ds_in_p/Stack/main.py
@@ -0,0 +1,11 @@
+from stack_class import Stack
+
+new_stack=Stack(4)
+new_stack.print_stack_contents()
+new_stack.push(50)
+new_stack.push(55)
+new_stack.print_stack_contents()
+new_stack.pop()
+new_stack.pop()
+new_stack.pop()
+new_stack.print_stack_contents()
