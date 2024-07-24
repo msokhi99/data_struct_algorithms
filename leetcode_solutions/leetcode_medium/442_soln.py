581471616e217861b7f23ca5f4abd2cdfb5f6449
Mantej Sokhi <sokhimantej99@gmail.com>
2024-07-24 16:11:54 -0700

    Updated 442_soln.

--- leetcode_solutions/leetcode_medium/442_soln.py
+++ leetcode_solutions/leetcode_medium/442_soln.py
@@ -3,7 +3,7 @@
 # Given an integer array nums of length n where all the integers of nums are in the range [1, n] and each integer appears once or twice, return an array of all the integers that appears twice.
 # You must write an algorithm that runs in O(n) time and uses only constant extra space.
 
-# Solution: 
+# Solution 1: 
 
 class Solution:
   def containsDups(self, nums: List[int]) -> List[int]:
@@ -17,3 +17,25 @@ class Solution:
         duplicate_elements.append(num)
 
     return duplicate_elements
+
+# Time Complexity: O(n).
+# Space Complexity: O(n).
+
+# Solution 2: (As required):
+
+class Solution:
+  def containsDups(self, nums: List[int]) -> List[int]:
+    result=[]
+
+    for num in nums:
+      index_to_check=abs(num)-1
+
+      if nums[index_to_check]<0:
+        result.append(abs(num))
+      else:
+        nums[index_to_check] = -nums[index_to_check]
+
+    return result
+
+# Time Complexity: O(n).
+# Space Complexity: O(1).
