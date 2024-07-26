dad24e279d604854e2535119aed3985c651a3879
Mantej Sokhi <sokhimantej99@gmail.com>
2024-07-26 14:26:49 -0700

    Added Soln. for 128.

---
+++ leetcode_solutions/leetcode_medium/128_soln.py
@@ -0,0 +1,25 @@
+# 128: 
+
+# Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
+# You must write an algorithm that runs in O(n) time.
+
+# Solution: Time Complexity: O(n): 
+
+class Solution:
+  def longest_seq(self, nums: List[int]) -> int:
+    num_set=set(nums)
+    longest_seq=0
+
+    for num in nums:
+      curr_len=1
+      prev_num=num-1
+      if prev_num not in num_set:
+        next_num=num+1
+        while next_num in num_set:
+          curr_len+=1
+          next_num+=1
+
+      longest_seq=max(longest_seq,curr_len)
+
+    return longest_seq
+
