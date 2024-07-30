711710b9d90d8b409937cecff11219ef843f87b8
Mantej Sokhi <sokhimantej99@gmail.com>
2024-07-29 23:16:05 -0700

    Added Soln. for 853.

---
+++ leetcode_solutions/leetcode_medium/853_soln.py
@@ -0,0 +1,30 @@
+# 853: 
+
+# There are n cars at given miles away from the starting mile 0, traveling to reach the mile target.
+# You are given two integer array position and speed, both of length n, where position[i] is the starting mile of the ith car and speed[i] is the speed of the ith car in miles per hour.
+# A car cannot pass another car, but it can catch up and then travel next to it at the speed of the slower car.
+# A car fleet is a car or cars driving next to each other. The speed of the car fleet is the minimum speed of any car in the fleet.
+# If a car catches up to a car fleet at the mile target, it will still be considered as part of the car fleet.
+# Return the number of car fleets that will arrive at the destination.
+
+# Solution:
+
+# Time Complexity: O(n*log(n)).
+# Space Complexity: O(n).
+
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        pos_and_speed=[(position[i],speed[i]) for i in range(len(position))]
+        pos_and_speed=sorted(pos_and_speed)
+        stack=[]
+
+        for i in range(len(position)-1,-1,-1):
+            car_pos=pos_and_speed[i][0]
+            car_speed=pos_and_speed[i][1]
+            time_to_reach_target=(target-car_pos)/car_speed
+            stack.append(time_to_reach_target)
+
+            if len(stack)>=2 and stack[-1]<=stack[-2]:
+                stack.pop()
+        
+        return len(stack)
