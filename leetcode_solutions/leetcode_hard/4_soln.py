0f86c938c26eb34dff61871798e1217809e315bc
Mantej Sokhi <sokhimantej99@gmail.com>
2024-08-01 20:57:42 -0700

    Updated Soln.

--- leetcode_solutions/leetcode_hard/4_soln.py
+++ leetcode_solutions/leetcode_hard/4_soln.py
@@ -3,7 +3,10 @@
 # Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
 # The overall run time complexity should be O(log (m+n)).
 
-# Solution: 
+# Solution 1 (Brute Force):
+# Time Complexity: O(m+n)
+# Space Complexity: O(m+n)
+
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
         merged_arr = []
@@ -33,3 +36,40 @@ class Solution:
             element_one=int((math.ceil(total_length)/2))
             result=float(merged_arr[element_one])
             return result
+
+# Solution 2: Optimal
+# Time Complexity: O(log(m+n))
+# Space Complexity: O(1)
+
+class Solution:
+    def median_arr(self,nums1:List[int],nums2:List[int])->float:
+        A=nums1
+        B=nums2
+        total_len=len(nums1)+len(nums2)
+        half_len=total_len//2
+
+        if len(B) < len(A):
+            A=B
+            B=A
+
+        left_in=0
+        right_in=len(A)-1
+
+        while True:
+            mid_a=(left_in+right_in)//2
+            mid_b=half_len-mid_a-2
+
+            a_left=A[mid_a] if mid_a>=0 else float("-infinity")
+            a_right=A[mid_a+1] if (mid_a+1)<len(A) else float("infinity")
+            b_left=B[mid_b] if mid_b>=0 else float("-infinity")
+            b_right=B[mid_b+1] if (mid_b+1)<len(B) else float("infinity")
+
+            if a_right<=b_left and b_right<=a_left:
+                if total_len % 2:
+                    return min(a_right,b_right)
+                else:
+                    return ((max(a_left,b_left)+min(a_right,b_right))/2)
+            elif a_left>b_right:
+                right_in=mid_a-1
+            else:
+                left_in=mid_a+1
