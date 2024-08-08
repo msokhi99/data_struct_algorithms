2c30e2ce940af73e81b937aafda5da0e27839295
Mantej Sokhi <sokhimantej99@gmail.com>
2024-08-08 14:50:19 -0700

    Added Iterative Soln.

--- leetcode_solutions/leetcode_easy/94_soln.py
+++ leetcode_solutions/leetcode_easy/94_soln.py
@@ -16,3 +16,24 @@ class Solution:
       result.append(node.val)
       self._recurse(node.right, result)
     return result
+
+# Iterative Solution: 
+
+class Solution:
+  def in_order_traversal(self, root:List[TreeNode])->List[int]:
+    result=[]
+    stack=[]
+    current=root
+
+    while stack or current:
+      while current:
+        stack.append(current)
+        current=current.left
+
+      current=stack.pop()
+      result.append(current.val)
+
+      current=current.right
+
+    return result
+
